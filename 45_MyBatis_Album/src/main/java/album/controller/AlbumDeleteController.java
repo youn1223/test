420e08c9c62115b9aedeb11f0d006001b58f1513
youn1223@daum.net <youn1223@daum.net>
2019-02-01 12:11:31 +0900

    'album-02-01'

--- 45_MyBatis_Album/src/main/java/album/controller/AlbumDeleteController.java
+++ 45_MyBatis_Album/src/main/java/album/controller/AlbumDeleteController.java
@@ -12,22 +12,6 @@ import album.model.AlbumDao;
 @Controller
 public class AlbumDeleteController {
 
-	private static final String gotoPage = "redirect:/list.ab";
-	private static final String command = "/delete.ab";
 	
-	@Autowired
-	private AlbumDao albumDao;
-	
-	@RequestMapping(value=command, method=RequestMethod.GET)
-	public String doActionGet(
-			@RequestParam(value="num",required=true) int num,
-			@RequestParam(value="pageNumber",required=true) int pageNumber){
-		
-		System.out.println("AlbumDeleteController Get 방식 들어옴");
-		int cnt=-1;
-		cnt = albumDao.DeleteAlbum(num);
-		
-		return gotoPage+"?pageNumber="+pageNumber; // redirect는 get방식 요청이다.
-	}
 	
 }
--- 45_MyBatis_Album/src/main/java/album/controller/AlbumDetailViewController.java
+++ 45_MyBatis_Album/src/main/java/album/controller/AlbumDetailViewController.java
@@ -16,36 +16,6 @@ public class AlbumDetailViewController {
 	private static final String getPage = "AlbumDetailView";
 	private static final String command = "/detail.ab";
 	
-	@Autowired
-	private AlbumDao albumDao; // AlbumDao의 객체가 들어옴
 	
-	@RequestMapping(value=command, method=RequestMethod.GET)
-	public String doActionGet(@RequestParam(value="num", required=true) int num,
-			@RequestParam(value="pageNumber", required=false) int pageNumber,
-			Model model){
-		System.out.println("Get 방식 들어옴");
-		System.out.println("pageNumber:"+pageNumber);
-		
-		AlbumBean album = albumDao.GetAlbum(num);
-		model.addAttribute("album",album);
-		model.addAttribute("pageNumber",pageNumber);
-		return getPage;		
-	}
-	/*위에서 pageNumber를 @RequestParam으로 받고 model로도 설정해야 보고있던 페이지로 넘어갈 수 있다.*/
-	
-	// 위의 코드도 되고, 아래 코드도 된다.
-	/*
-	@RequestMapping(value=command, method=RequestMethod.GET)
-	public ModelAndView doActionGet(@RequestParam(value="num", required=true) int num){
-		
-		System.out.println("Get 방식 들어옴");
-		AlbumBean album = albumDao.GetAlbum(num);
-		
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("album",album);
-		mav.setViewName(getPage);
-		return mav;
-	}
-	*/
 	
 }
